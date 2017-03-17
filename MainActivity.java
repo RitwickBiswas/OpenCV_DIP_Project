a947b0ee74b4e014cac6136040752d9305a55efd
Ritwick Biswas <ritwick.biswas95@gmail.com>
2017-03-18 00:13:14 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.ritwick.opencvtest2;
+
+import android.nfc.Tag;
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.MatOfInt;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.imgproc.Imgproc;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity implements CvCameraViewListener2{
+
+
+    private JavaCameraView mOpenCvCameraView;
+
+
+
+    private BaseLoaderCallback mBaseloaderCallBack = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status){
+                case LoaderCallbackInterface.SUCCESS:{
+                    mOpenCvCameraView.enableView();
+                    break;
+                }
+                default:{
+                    super.onManagerConnected(status);
+                }
+            }
+
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mOpenCvCameraView = (JavaCameraView) findViewById(R.id.MainActivityCamera);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+    }
+
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0,this,mBaseloaderCallBack);
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        if(mOpenCvCameraView != null){
+            mOpenCvCameraView.disableView();
+        }
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+
+        return inputFrame.gray();
+    }
+}
